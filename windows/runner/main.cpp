ea71a282657d241cc5027bf11dbb875fb0e81af1
Tory <exceedutow@gmail.com>
2025-12-26 18:21:01 +0800

    fix: 修复 Flutter 3.38 Windows 线程合并导致的性能问题

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -17,6 +17,11 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   ::CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
 
   flutter::DartProject project(L"data");
+  
+  // TODO: Remove this when Flutter fixes the performance regression.
+  // This forces Flutter to use a separate thread for Dart.
+  // See: https://github.com/flutter/flutter/issues/178916
+  project.set_ui_thread_policy(flutter::UIThreadPolicy::RunOnSeparateThread);
 
   std::vector<std::string> command_line_arguments =
       GetCommandLineArguments();
